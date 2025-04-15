cf269290180c5b975ba657821ebdc1043dca502a
Ron Marche Rhyss Q. Uy <you@example.com>
2025-04-15 09:32:40 +0800

    commit this

--- app/data/rooms.ts
+++ app/data/rooms.ts
@@ -3,35 +3,40 @@ export const topRatedRooms = [
     title: "Superior Room",
     price: 3200,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/superior-room.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677524/Rectangle_4170_oxttii.png",
     href: "/rooms/superior",
   },
   {
     title: "Business Elite Room",
     price: 4500,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/business-elite.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677523/Rectangle_4179_diz8hh.png",
     href: "/rooms/business-elite",
   },
   {
     title: "Family Haven Suite",
     price: 8000,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/family-haven.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677522/Rectangle_4180_lptud5.png",
     href: "/rooms/family-haven",
   },
   {
     title: "Grand Royale Room",
     price: 18000,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/grand-royale.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677522/Rectangle_4181_d5siz2.png",
     href: "/rooms/grand-royale",
   },
   {
     title: "Imperial Palace Suite",
     price: 25000,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/imperial-palace.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677522/Rectangle_4182_f1afdf.png",
     href: "/rooms/imperial-palace",
   },
 ];
@@ -41,35 +46,40 @@ export const categoryRooms = [
     title: "Standard Rooms",
     price: 2800,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/standard.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677523/Rectangle_4170_1_rbpgkp.png",
     href: "/category/standard",
   },
   {
     title: "Executive / Business Rooms",
     price: 3800,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/executive.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744678121/Rectangle_4179_1_ccdrgd.png",
     href: "/category/executive",
   },
   {
     title: "Family-Friendly Rooms",
     price: 9500,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/family.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677523/Rectangle_4180_1_ucplwd.png",
     href: "/category/family",
   },
   {
     title: "Luxury Rooms",
     price: 15000,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/luxury.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677522/Rectangle_4181_1_xvlrwu.png",
     href: "/category/luxury",
   },
   {
     title: "Palace-Inspired / Royal Suites",
     price: 30000,
     location: "Taguig, Metro Manila",
-    imageUrl: "/rooms/palace.jpg",
+    imageUrl:
+      "https://res.cloudinary.com/ddnxfpziq/image/upload/v1744677522/Rectangle_4182_1_n68tmf.png",
     href: "/category/palace",
   },
 ];
