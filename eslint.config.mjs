a49fbee1a78f8bc6ce6084320fb0b7b4634c8a39
Ronmarche1114 <ronmarcherhyssquy@gmail.com>
2025-05-14 04:20:41 +0800

    m

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig } from 'eslint-define-config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,18 +13,16 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default defineConfig({
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next/core-web-vitals',
-  ],
-  ignorePatterns: ['node_modules/', '.next/'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react/no-unescaped-entities': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@next/next/no-img-element': 'warn',
-  },
-});
+export default [
+  ...eslintConfig,
+  {
+    ignorePatterns: ['node_modules/', '.next/'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
+    },
+  }
+];
