91d95b79640ac46ec47f99516f1e01624fa58929
Ronmarche1114 <ronmarcherhyssquy@gmail.com>
2025-05-15 13:30:48 +0800

    commit now

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -11,7 +11,7 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 type User = {
   id: string;
